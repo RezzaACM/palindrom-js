d070b0b859fba99ffa5b08c39c18ce7352b10d9f
RezzaACM <48206768+RezzaACM@users.noreply.github.com>
2021-05-18 20:29:30 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+
+function palindrome(str) {
+  let re = /[^A-Za-z0-9]/g;
+  str = str.toLowerCase().replace(re, "");
+  let len = str.length;
+  for (let i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(palindrome("katak"));
